aaadc010e65ff989af38cc9e128b6ad1761e3bd6
JohnnyBackflip <johnnybackflip@gmail.com>
2026-03-31 13:55:13 +0200

    Remove header and menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { AgeVerification } from './components/age-verification/age-verification';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, AgeVerification],
+  imports: [RouterOutlet, Footer, AgeVerification],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
